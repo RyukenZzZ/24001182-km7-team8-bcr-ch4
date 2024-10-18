91ff6297b577aaf371d574cbc75e5689459dd745
chawnima <mferdyalviansyah@gmail.com>
2024-10-18 09:24:16 +0700

    Revert "fix conflict"
    This reverts commit 1d8163f5924925bfe4a1e295f3604c624bfba5bb, reversing
    changes made to c5fdf37721a36a1c5f82382e1e0e10f5652da274.

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,8 @@
 const express = require("express");
-
-const carsRouter = require("./cars");
-const modelsRouter = require("./models");
-const typeRouter = require("./type");
+const carsRouter = require("./type");
 
 const router = express.Router();
 
-router.use("/cars", carsRouter);
-router.use("/models", modelsRouter);
-router.use("/type", typeRouter);
+router.use("/type", carsRouter);
 
 module.exports = router;
