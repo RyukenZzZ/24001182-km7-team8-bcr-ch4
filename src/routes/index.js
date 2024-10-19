71606ebd69412c4129a81d1681ab9b90a78d8162
chawnima <mferdyalviansyah@gmail.com>
2024-10-19 15:27:58 +0700

    fix commented line

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,14 @@
 const express = require("express");
 
 const carsRouter = require("./cars");
-//const modelsRouter = require("./models");
+const modelsRouter = require("./models");
 const typeRouter = require("./type");
 const manufacturesRouter = require("./manufactures");
 
 const router = express.Router();
 
 router.use("/cars", carsRouter);
-//router.use("/models", modelsRouter);
+router.use("/models", modelsRouter);
 router.use("/type", typeRouter);
 router.use("/manufactures", manufacturesRouter);
 
