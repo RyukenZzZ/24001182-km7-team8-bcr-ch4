35e57b7bec2ec16ba8fe653b193b4a86b1b86a9a
“RyukenZzZ” <zxryuken@gmail.com>
2024-10-19 14:55:44 +0700

    feature: Read,Update,Delete

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,8 @@
 const express = require("express");
-const carsRouter = require("./cars");
 const modelsRouter = require("./models")
 
 const router = express.Router();
 
-router.use("/cars", carsRouter);
 router.use("/models", modelsRouter);
 
 module.exports = router;
